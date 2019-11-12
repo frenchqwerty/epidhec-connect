2b775679d496fd937f81c3a34c46ec9f580c659e
Eric Deschodt <eric.deschodt@exosuite.fr>
2019-11-12 19:55:03 +0100

    Ajout de la position de base des bornes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,10 +11,20 @@ export class AppComponent {
   title = 'epidhec-connect';
   maxBounds = new LngLatBounds([0.08, 49.46], [0.20, 49.50]);
 
-  instantiateBornes = (number: number) => {
-    let bornes = [];
-    for (let i = 0; i < number; i++)
-      bornes.push(new Borne(new LngLat(0.14, 49.48), "borne 1"))
+  findNextBorne = (num: number) => {
+    const positions = [];
+    positions.push(new LngLat(49.478897, 0.125546));
+    positions.push(new LngLat(49.481757, 0.108243));
+    positions.push(new LngLat(49.485081, 0.116746));
+    positions.push(new LngLat(49.491879, 0.093954));
+    positions.push(new LngLat(49.467266, 0.189413));
+    return (new Borne(positions[num], 'borne no ' + num.toString()));
+  }
+
+  instantiateBornes = () => {
+    const bornes = [];
+    for(let i = 0; i < 5; i++)
+      bornes.push(this.findNextBorne(i))
     return bornes;
   };
   sourceBorne = {
@@ -31,5 +41,5 @@ export class AppComponent {
     }
   };
 
-  bornes = this.instantiateBornes(1);
+  bornes = this.instantiateBornes();
 }
