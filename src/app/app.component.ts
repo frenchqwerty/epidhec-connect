4fc48a300d80d3acd97e1508a36bf7382c0a0142
Yassir <yassir.jabbari@epitech.eu>
2019-11-12 22:18:37 +0100

    Place borne

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {LngLat, LngLatBounds} from "mapbox-gl";
 import {Borne} from "./model/borne";
 
@@ -10,36 +10,72 @@ import {Borne} from "./model/borne";
 export class AppComponent {
   title = 'epidhec-connect';
   maxBounds = new LngLatBounds([0.08, 49.46], [0.20, 49.50]);
-
-  findNextBorne = (num: number) => {
-    const positions = [];
-    positions.push(new LngLat(49.478897, 0.125546));
-    positions.push(new LngLat(49.481757, 0.108243));
-    positions.push(new LngLat(49.485081, 0.116746));
-    positions.push(new LngLat(49.491879, 0.093954));
-    positions.push(new LngLat(49.467266, 0.189413));
-    return (new Borne(positions[num], 'borne no ' + num.toString()));
-  }
-
-  instantiateBornes = () => {
-    const bornes = [];
-    for(let i = 0; i < 5; i++)
-      bornes.push(this.findNextBorne(i))
-    return bornes;
-  };
-  sourceBorne = {
-    type: "geojson",
-    data: {
-      type: "FeatureCollection",
-      features: [{
-        type: "Feature",
-        geometry: {
-          type: "Point",
-          coordinates: [0.14, 49.48]
+  positions = {
+    'type': 'geojson',
+    'data': {
+      'type': 'FeatureCollection',
+      'features': [
+        {
+          'type': "Feature",
+          'geometry': {
+            'type': 'Point',
+            'coordinates': [0.125546, 49.478897]
+          },
+          'properties': {
+            'marker-color': '#3bb2d0',
+            'marker-size': 'large',
+            'marker-symbol': '1'
+          }
+        },
+        {
+          'type': "Feature",
+          'geometry': {
+            'type': 'Point',
+            'coordinates': [0.108243, 49.481757]
+          },
+          'properties': {
+            'marker-color': '#3bb2d0',
+            'marker-size': 'large',
+            'marker-symbol': '2'
+          }
+        },
+        {
+          'type': "Feature",
+          'geometry': {
+            'type': 'Point',
+            'coordinates': [0.116746, 49.485081]
+          },
+          'properties': {
+            'marker-color': '#3bb2d0',
+            'marker-size': 'large',
+            'marker-symbol': '3'
+          }
+        },
+        {
+          'type': "Feature",
+          'geometry': {
+            'type': 'Point',
+            'coordinates': [0.093954, 49.491879]
+          },
+          'properties': {
+            'marker-color': '#3bb2d0',
+            'marker-size': 'large',
+            'marker-symbol': '4'
+          }
+        },
+        {
+          'type': "Feature",
+          'geometry': {
+            'type': 'Point',
+            'coordinates': [0.189413, 49.467266]
+          },
+          'properties': {
+            'marker-color': '#3bb2d0',
+            'marker-size': 'large',
+            'marker-symbol': '5'
+          }
         }
-      }]
+      ]
     }
   };
-
-  bornes = this.instantiateBornes();
 }
