c1619433049d9da6099b7dc460175e18997cf32a
Yassir <yassir.jabbari@epitech.eu>
2019-12-07 17:07:35 +0100

    ChartJS

---
+++ donut.js
@@ -0,0 +1,387 @@
+let config1 = {
+    type: 'doughnut',
+    data: {
+        datasets: [{
+            data: [
+                Math.floor(Math.random() * (15 - 10 + 1) ) + 10,
+                Math.floor(Math.random() * (15 - 10 + 1) ) + 10,
+                Math.floor(Math.random() * (15 - 10 + 1) ) + 10,
+            ],
+            backgroundColor: [
+                '#00ff51',
+                '#b5ffcd',
+                '#006621'
+            ],
+        }],
+        labels: [
+            'Slice 1',
+            'Slice 2',
+            'Slice 3'
+        ]
+    },
+    options: {
+        responsive: true,
+        legend: {
+            position: 'top',
+        },
+        title: {
+            display: true,
+            text: 'Borne 1'
+        },
+        animation: {
+            animateScale: true,
+            animateRotate: true
+        }
+    }
+};
+let config2 = {
+    type: 'doughnut',
+    data: {
+        datasets: [{
+            data: [
+                Math.floor(Math.random() * (15 - 10 + 1) ) + 10,
+                Math.floor(Math.random() * (15 - 10 + 1) ) + 10,
+                Math.floor(Math.random() * (15 - 10 + 1) ) + 10,
+            ],
+            backgroundColor: [
+                '#00ff51',
+                '#b5ffcd',
+                '#006621'
+            ],
+        }],
+        labels: [
+            'Slice 1',
+            'Slice 2',
+            'Slice 3'
+        ]
+    },
+    options: {
+        responsive: true,
+        legend: {
+            position: 'top',
+        },
+        title: {
+            display: true,
+            text: 'Borne 2'
+        },
+        animation: {
+            animateScale: true,
+            animateRotate: true
+        }
+    }
+};
+let config3 = {
+    type: 'doughnut',
+    data: {
+        datasets: [{
+            data: [
+                Math.floor(Math.random() * (15 - 10 + 1) ) + 10,
+                Math.floor(Math.random() * (15 - 10 + 1) ) + 10,
+                Math.floor(Math.random() * (15 - 10 + 1) ) + 10,
+            ],
+            backgroundColor: [
+                '#00ff51',
+                '#b5ffcd',
+                '#006621'
+            ],
+        }],
+        labels: [
+            'Slice 1',
+            'Slice 2',
+            'Slice 3'
+        ]
+    },
+    options: {
+        responsive: true,
+        legend: {
+            position: 'top',
+        },
+        title: {
+            display: true,
+            text: 'Borne 3'
+        },
+        animation: {
+            animateScale: true,
+            animateRotate: true
+        }
+    }
+};
+let config4 = {
+    type: 'doughnut',
+    data: {
+        datasets: [{
+            data: [
+                10,
+                10,
+                10,
+            ],
+            backgroundColor: [
+                '#00ff51',
+                '#b5ffcd',
+                '#006621'
+            ],
+        }],
+        labels: [
+            'Slice 1',
+            'Slice 2',
+            'Slice 3'
+        ]
+    },
+    options: {
+        responsive: true,
+        legend: {
+            position: 'top',
+        },
+        title: {
+            display: true,
+            text: 'Borne 4'
+        },
+        animation: {
+            animateScale: true,
+            animateRotate: true
+        }
+    }
+};
+let config5 = {
+    type: 'doughnut',
+    data: {
+        datasets: [{
+            data: [
+                Math.floor(Math.random() * (15 - 10 + 1) ) + 10,
+                Math.floor(Math.random() * (15 - 10 + 1) ) + 10,
+                Math.floor(Math.random() * (15 - 10 + 1) ) + 10,
+            ],
+            backgroundColor: [
+                '#00ff51',
+                '#b5ffcd',
+                '#006621'
+            ],
+        }],
+        labels: [
+            'Slice 1',
+            'Slice 2',
+            'Slice 3'
+        ]
+    },
+    options: {
+        responsive: true,
+        legend: {
+            position: 'top',
+        },
+        title: {
+            display: true,
+            text: 'Borne 5'
+        },
+        animation: {
+            animateScale: true,
+            animateRotate: true
+        }
+    }
+};
+let config6 = {
+    type: 'doughnut',
+    data: {
+        datasets: [{
+            data: [
+                Math.floor(Math.random() * (15 - 10 + 1) ) + 10,
+                Math.floor(Math.random() * (15 - 10 + 1) ) + 10,
+                Math.floor(Math.random() * (15 - 10 + 1) ) + 10,
+            ],
+            backgroundColor: [
+                '#00ff51',
+                '#b5ffcd',
+                '#006621'
+            ],
+        }],
+        labels: [
+            'Slice 1',
+            'Slice 2',
+            'Slice 3'
+        ]
+    },
+    options: {
+        responsive: true,
+        legend: {
+            position: 'top',
+        },
+        title: {
+            display: true,
+            text: 'Borne 6'
+        },
+        animation: {
+            animateScale: true,
+            animateRotate: true
+        }
+    }
+};
+let config7 = {
+    type: 'doughnut',
+    data: {
+        datasets: [{
+            data: [
+                Math.floor(Math.random() * (15 - 10 + 1) ) + 10,
+                Math.floor(Math.random() * (15 - 10 + 1) ) + 10,
+                Math.floor(Math.random() * (15 - 10 + 1) ) + 10,
+            ],
+            backgroundColor: [
+                '#00ff51',
+                '#b5ffcd',
+                '#006621'
+            ],
+        }],
+        labels: [
+            'Slice 1',
+            'Slice 2',
+            'Slice 3'
+        ]
+    },
+    options: {
+        responsive: true,
+        legend: {
+            position: 'top',
+        },
+        title: {
+            display: true,
+            text: 'Borne 7'
+        },
+        animation: {
+            animateScale: true,
+            animateRotate: true
+        }
+    }
+};
+let config8 = {
+    type: 'doughnut',
+    data: {
+        datasets: [{
+            data: [
+                Math.floor(Math.random() * (15 - 10 + 1) ) + 10,
+                Math.floor(Math.random() * (15 - 10 + 1) ) + 10,
+                Math.floor(Math.random() * (15 - 10 + 1) ) + 10,
+            ],
+            backgroundColor: [
+                '#00ff51',
+                '#b5ffcd',
+                '#006621'
+            ],
+        }],
+        labels: [
+            'Slice 1',
+            'Slice 2',
+            'Slice 3'
+        ]
+    },
+    options: {
+        responsive: true,
+        legend: {
+            position: 'top',
+        },
+        title: {
+            display: true,
+            text: 'Borne 8'
+        },
+        animation: {
+            animateScale: true,
+            animateRotate: true
+        }
+    }
+};
+let config9 = {
+    type: 'doughnut',
+    data: {
+        datasets: [{
+            data: [
+                Math.floor(Math.random() * (15 - 10 + 1) ) + 10,
+                Math.floor(Math.random() * (15 - 10 + 1) ) + 10,
+                Math.floor(Math.random() * (15 - 10 + 1) ) + 10,
+            ],
+            backgroundColor: [
+                '#00ff51',
+                '#b5ffcd',
+                '#006621'
+            ],
+        }],
+        labels: [
+            'Slice 1',
+            'Slice 2',
+            'Slice 3'
+        ]
+    },
+    options: {
+        responsive: true,
+        legend: {
+            position: 'top',
+        },
+        title: {
+            display: true,
+            text: 'Borne 9'
+        },
+        animation: {
+            animateScale: true,
+            animateRotate: true
+        }
+    }
+};
+let config10 = {
+    type: 'doughnut',
+    data: {
+        datasets: [{
+            data: [
+                Math.floor(Math.random() * (15 - 10 + 1) ) + 10,
+                Math.floor(Math.random() * (15 - 10 + 1) ) + 10,
+                Math.floor(Math.random() * (15 - 10 + 1) ) + 10,
+            ],
+            backgroundColor: [
+                '#00ff51',
+                '#b5ffcd',
+                '#006621'
+            ],
+        }],
+        labels: [
+            'Slice 1',
+            'Slice 2',
+            'Slice 3'
+        ]
+    },
+    options: {
+        responsive: true,
+        legend: {
+            position: 'top',
+        },
+        title: {
+            display: true,
+            text: 'Borne 10'
+        },
+        animation: {
+            animateScale: true,
+            animateRotate: true
+        }
+    }
+};
+window.onload = function () {
+    const ctx1 = document.getElementById('borne1').getContext('2d');
+    const ctx2 = document.getElementById('borne2').getContext('2d');
+    const ctx3 = document.getElementById('borne3').getContext('2d');
+    const ctx4 = document.getElementById('borne4').getContext('2d');
+    const ctx5 = document.getElementById('borne5').getContext('2d');
+    const ctx6 = document.getElementById('borne6').getContext('2d');
+    const ctx7 = document.getElementById('borne7').getContext('2d');
+    const ctx8 = document.getElementById('borne8').getContext('2d');
+    const ctx9 = document.getElementById('borne9').getContext('2d');
+    const ctx10 = document.getElementById('borne10').getContext('2d');
+    window.borne1 = new Chart(ctx1, config1);
+    window.borne2 = new Chart(ctx2, config2);
+    window.borne3 = new Chart(ctx3, config3);
+    window.borne4 = new Chart(ctx4, config4);
+    window.borne5 = new Chart(ctx5, config5);
+    window.borne6 = new Chart(ctx6, config6);
+    window.borne7 = new Chart(ctx7, config7);
+    window.borne8 = new Chart(ctx8, config8);
+    window.borne9 = new Chart(ctx9, config9);
+    window.borne10 = new Chart(ctx10, config10);
+    updateConfigAsNewObject(window.borne1);
+};
+
+function updateConfigAsNewObject(chart, newData) {
+    chart.update();
+}
